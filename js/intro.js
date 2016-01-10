fbf1223621850d704180d29976690ec45639dd5d
mbj36 <mohitbajo36@gmail.com>
2016-01-10 23:17:20 +0530

    Intro page

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,2 @@
-var app=angular.module("MyApp",[]);
-app.controller('IntroCtrl',function($scope, config)
-               {
-});
-    
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
